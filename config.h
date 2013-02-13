1b523578db618ce4674efc60d1515484b02bb464
Andrew Byrd <andrew@fastmail.net>
2013-02-13 21:30:04 +0100

    test (use current directory)

--- config.h
+++ config.h
@@ -2,8 +2,8 @@
 
 #define RRRR_TEST_CONCURRENCY 4
 #define RRRR_TEST_REQUESTS 500
-#define RRRR_INPUT_FILE "./timetable.dat"
-#define RRRR_MAX_ROUNDS 6
+#define RRRR_INPUT_FILE "/home/abyrd/git/rrrr/timetable.dat"
+#define RRRR_MAX_ROUNDS 5
 
 // bind does not work with names (localhost) but does work with *
 #define CLIENT_ENDPOINT "tcp://127.0.0.1:9292"
