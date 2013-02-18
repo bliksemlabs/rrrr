1ddba35da46d98d5fb29fe22bd1d650951fbc621
Andrew Byrd <andrew@fastmail.net>
2013-02-18 10:02:29 +0100

    correct rrrr.sh script for new rand/id parameter

--- rrrr.sh
+++ rrrr.sh
@@ -8,7 +8,7 @@ echo starting new processes
 # 8 workers is not significantly faster than 4 on a 4-core machine (confirmed twice)
 for i in {1..4} ; do ./workerrrr & done
 #sleep 1
-./client 1 1
+./client rand 1 1
 # old fcgi:
 # cgi-fcgi -start -connect /tmp/fastcgi.socket ./rrrr $NUM_HANDLERS
 # chmod a+rw /tmp/fastcgi.socket
