7d472a60e3a9cd54792966ebbfce80826e1cbfcf
Andrew Byrd <andrew@fastmail.net>
2013-07-17 10:35:14 +0200

    switch bitshift macros to >>2, clean up comments, add sec_in_day defines

--- util.h
+++ util.h
@@ -6,16 +6,22 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-// 2**16 / 60 / 60 is 18.2 hours at one-second resolution.
-// by right-shifting times one bit, we get 36.4 hours (over 1.5 days) at 2 second resolution.
-// by right-shifting times two bits, we get 72.8 hours (over 3 days) at 4 second resolution.
-// Three days is just enough to model yesterday, today, and tomorrow for overnight searches,
-// and can also represent the longest rail journeys in Europe.
+/*
+  2^16 / 60 / 60 is 18.2 hours at one-second resolution.
+  By right-shifting times one bit, we get 36.4 hours (over 1.5 days) at 2 second resolution.
+  By right-shifting times two bits, we get 72.8 hours (over 3 days) at 4 second resolution.
+  Three days is just enough to model yesterday, today, and tomorrow for overnight searches,
+  and can also represent the longest rail journeys in Europe.
+*/
 typedef uint16_t rtime_t;
 
-#define SEC_TO_RTIME(x) (x >> 1)
-#define RTIME_TO_SEC(x) (x << 1)
-#define RTIME_ONE_DAY (RTIME_FROM_SEC(24 * 60 * 60))
+#define SEC_TO_RTIME(x) ((x) >> 2)
+#define RTIME_TO_SEC(x) ((x) << 2)
+
+#define SEC_IN_ONE_DAY  (24 * 60 * 60)
+#define SEC_IN_TWO_DAYS (2 * SEC_IN_ONE_DAY)
+#define RTIME_ONE_DAY (RTIME_FROM_SEC(SEC_IN_ONE_DAY))
+
 
 // We should avoid relying on the relative value of these preprocessor constants (inequalities)
 // since they will be used in both departAfter and arriveBy searches.
