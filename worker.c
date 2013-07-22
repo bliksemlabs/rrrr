d4d710b7b7dfd7889702318b21b36da1f7edee9c
Andrew Byrd <andrew@fastmail.net>
2013-07-22 16:33:54 +0200

    disable search reversal while working on overnight routing

--- worker.c
+++ worker.c
@@ -59,6 +59,7 @@ int main(int argc, char **argv) {
             router_route (&router, &req);
             // repeat search in reverse to compact transfers
             uint32_t n_reversals = req.arrive_by ? 1 : 2;
+            n_reversals = 0; // DEBUG turn off reversals
             for (uint32_t i = 0; i < n_reversals; ++i) {
                 router_request_reverse (&router, &req); // handle case where route is not reversed
                 D printf ("Repeating search with reversed request: \n");
