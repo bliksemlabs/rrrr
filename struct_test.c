e0249ed9c9dd8e6bf0b3bbad75347a26d73dbb0e
Andrew Byrd <andrew@fastmail.net>
2013-08-18 15:50:03 +0200

    demonstrate size effects of removing board_time

--- struct_test.c
+++ struct_test.c
@@ -42,12 +42,25 @@ struct s3 {
     rtime_t  walk_time;  // The time when this stop was reached by walking (2nd phase)
 };
 
-void main () {
-    int n_stops = 50000;
-    int n_rounds = 8;
+struct s4 {
+    /* First phase: transit ride results */
+    /* Second phase: transfer results */
+    uint32_t back_stop;  // The index of the previous stop in the itinerary
+    uint32_t back_route; // The index of the route used to travel from back_stop to here, or WALK
+    uint32_t back_trip;  // The index of the trip used to travel from back_stop to here, or WALK
+    uint32_t walk_from;  // The stop from which this stop was reached by walking (2nd phase)
+    rtime_t  time;       // The time when this stop was reached
+    rtime_t  walk_time;  // The time when this stop was reached by walking (2nd phase)
+};
+
+int main () {
+    int n_stops = 60000;
+    int n_rounds = 5;
     printf ("size = %ld total size = %ld \n", sizeof(struct s0), n_stops * n_rounds * sizeof(struct s0));
     printf ("size = %ld total size = %ld \n", sizeof(struct s1), n_stops * n_rounds * sizeof(struct s1));
     printf ("size = %ld total size = %ld \n", sizeof(struct s2), n_stops * n_rounds * sizeof(struct s2));
     printf ("size = %ld total size = %ld \n", sizeof(struct s3), n_stops * n_rounds * sizeof(struct s3));
+    printf ("size = %ld total size = %ld \n", sizeof(struct s4), n_stops * n_rounds * sizeof(struct s4));
+    return 0;
 }
 
