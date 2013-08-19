92f8f09b47258e1d0a72d4d99cf03c154f34b1dd
Andrew Byrd <andrew@fastmail.net>
2013-08-19 03:07:56 +0200

    whitespace: tabs to spaces

--- realtime.c
+++ realtime.c
@@ -88,73 +88,73 @@ static bool socket_closed = false;
 static bool force_exit = false;
  
 static int callback_gtfs_rt (struct libwebsocket_context *this,
-			                 struct libwebsocket *wsi,
-			                 enum libwebsocket_callback_reasons reason,
+                             struct libwebsocket *wsi,
+                             enum libwebsocket_callback_reasons reason,
                              void *user, void *in, size_t len) {
-	switch (reason) {
+    switch (reason) {
 
-	case LWS_CALLBACK_CLOSED:
-		fprintf(stderr, "LWS_CALLBACK_CLOSED\n");
-		socket_closed = 1;
-		break;
+    case LWS_CALLBACK_CLOSED:
+        fprintf(stderr, "LWS_CALLBACK_CLOSED\n");
+        socket_closed = 1;
+        break;
 
-	case LWS_CALLBACK_CLIENT_RECEIVE:
-	    fprintf(stderr, "rx %d bytes: ", (int)len);
-	    if (libwebsockets_remaining_packet_payload (wsi)) {
-	        fprintf (stderr, "frame exceeds maximum allowed frame length\n");
-	    } else if (libwebsocket_is_final_fragment (wsi)) {
-    	    fprintf(stderr, "final frame, ");
-	        if (msg_len == 0) {
-	            /* single frame message, nothing in the buffer */
-        	    fprintf(stderr, "single-frame message. ");
-	            if (len > 0) show_gtfsrt (in, len);
+    case LWS_CALLBACK_CLIENT_RECEIVE:
+        fprintf(stderr, "rx %d bytes: ", (int)len);
+        if (libwebsockets_remaining_packet_payload (wsi)) {
+            fprintf (stderr, "frame exceeds maximum allowed frame length\n");
+        } else if (libwebsocket_is_final_fragment (wsi)) {
+            fprintf(stderr, "final frame, ");
+            if (msg_len == 0) {
+                /* single frame message, nothing in the buffer */
+                fprintf(stderr, "single-frame message. ");
+                if (len > 0) show_gtfsrt (in, len);
             } else { 
                 /* last frame in a multi-frame message */
-        	    fprintf(stderr, "had previous fragment frames. ");
+                fprintf(stderr, "had previous fragment frames. ");
                 msg_add_frame (in, len);
                 // msg_dump (); // visualize assembled message
                 show_gtfsrt (msg, msg_len);
-        	    fprintf(stderr, "emptying message buffer. ");
+                fprintf(stderr, "emptying message buffer. ");
                 msg_reset();
             }
-	    } else {
-    	    /* non-final fragment frame */
-    	    fprintf(stderr, "message fragment frame. ");
+        } else {
+            /* non-final fragment frame */
+            fprintf(stderr, "message fragment frame. ");
             msg_add_frame (in, len);
         }
-	    fprintf(stderr, "\n");
-		break;
+        fprintf(stderr, "\n");
+        break;
 
-	case LWS_CALLBACK_CLIENT_CONFIRM_EXTENSION_SUPPORTED:
+    case LWS_CALLBACK_CLIENT_CONFIRM_EXTENSION_SUPPORTED:
         return 0; // say everything is supported?
-		break;
+        break;
 
-	default:
-		break;
-	}
-	return 0;
+    default:
+        break;
+    }
+    return 0;
 }
 
 
 /* List of supported protocols and their callbacks */
 
 enum protocols {
-	PROTOCOL_INCREMENTAL_GTFS_RT,
-	PROTOCOL_COUNT /* end */
+    PROTOCOL_INCREMENTAL_GTFS_RT,
+    PROTOCOL_COUNT /* end */
 };
 
 static struct libwebsocket_protocols protocols[] = {
-	{
-		"incremental-gtfs-rt",
-		callback_gtfs_rt,
-		0,                 // shared protocol data size
-		MAX_FRAME_LENGTH,  // frame buffer size
-	},
-	{ NULL, NULL, 0, 0 } /* end */
+    {
+        "incremental-gtfs-rt",
+        callback_gtfs_rt,
+        0,                 // shared protocol data size
+        MAX_FRAME_LENGTH,  // frame buffer size
+    },
+    { NULL, NULL, 0, 0 } /* end */
 };
 
 void sighandler(int sig) {
-	force_exit = true;
+    force_exit = true;
 }
 
 static struct option long_options[] = {
@@ -167,70 +167,70 @@ static struct option long_options[] = {
 
 
 int main(int argc, char **argv) {
-	int ret = 0;
-	int port = 8088;
-	bool use_ssl = false;
-	struct libwebsocket_context *context;
-	const char *address = "ovapi.nl";
-	const char *path = "/vehiclePositions";
-	struct libwebsocket *wsi_gtfsrt;
-	int ietf_version = -1; /* latest */
-	struct lws_context_creation_info cc_info;
+    int ret = 0;
+    int port = 8088;
+    bool use_ssl = false;
+    struct libwebsocket_context *context;
+    const char *address = "ovapi.nl";
+    const char *path = "/vehiclePositions";
+    struct libwebsocket *wsi_gtfsrt;
+    int ietf_version = -1; /* latest */
+    struct lws_context_creation_info cc_info;
 
     int opt = 0;
-	while (opt >= 0) {
-		opt = getopt_long(argc, argv, "p:a:h", long_options, NULL);
-		if (opt < 0) continue;
-		switch (opt) {
-		case 'p':
-			port = atoi(optarg);
-			break;
-		case 'a':
-			address = optarg;
-		    printf ("address set to %s\n", address);
-			break;
-		case 't':
-			path = optarg;
-		    printf ("path set to %s\n", path);
-			break;
-		case 'h':
-			goto usage;
-		}
-	}
+    while (opt >= 0) {
+        opt = getopt_long(argc, argv, "p:a:h", long_options, NULL);
+        if (opt < 0) continue;
+        switch (opt) {
+        case 'p':
+            port = atoi(optarg);
+            break;
+        case 'a':
+            address = optarg;
+            printf ("address set to %s\n", address);
+            break;
+        case 't':
+            path = optarg;
+            printf ("path set to %s\n", path);
+            break;
+        case 'h':
+            goto usage;
+        }
+    }
 
-	signal(SIGINT, sighandler);
+    signal(SIGINT, sighandler);
 
-	/*
-	 * create the websockets context.  This tracks open connections and
-	 * knows how to route any traffic and which protocol version to use,
-	 * and if each connection is client or server side.
-	 * This is client-only, so we tell it to not listen on any port.
-	 */
+    /*
+     * create the websockets context.  This tracks open connections and
+     * knows how to route any traffic and which protocol version to use,
+     * and if each connection is client or server side.
+     * This is client-only, so we tell it to not listen on any port.
+     */
     memset (&cc_info, 0, sizeof cc_info);
     cc_info.port = CONTEXT_PORT_NO_LISTEN;
     cc_info.protocols = protocols;
-	cc_info.extensions = libwebsocket_get_internal_extensions();
-	cc_info.gid = -1;
-	cc_info.uid = -1;
-	context = libwebsocket_create_context (&cc_info);
-	if (context == NULL) {
-		fprintf (stderr, "creating libwebsocket context failed\n");
-		return 1;
-	}
+    cc_info.extensions = libwebsocket_get_internal_extensions();
+    cc_info.gid = -1;
+    cc_info.uid = -1;
+    context = libwebsocket_create_context (&cc_info);
+    if (context == NULL) {
+        fprintf (stderr, "creating libwebsocket context failed\n");
+        return 1;
+    }
 
-	/* create a client websocket to incremental gtfs-rt distributor */
+    /* create a client websocket to incremental gtfs-rt distributor */
     wsi_gtfsrt = libwebsocket_client_connect (context, address, port, use_ssl, path, address, address,
                  protocols[PROTOCOL_INCREMENTAL_GTFS_RT].name, ietf_version);
-	if (wsi_gtfsrt == NULL) {
-		fprintf(stderr, "libwebsocket gtfs-rt connect failed\n");
-		ret = 1;
-		goto bail;
-	}
-	fprintf(stderr, "websocket connections opened\n");
+    if (wsi_gtfsrt == NULL) {
+        fprintf(stderr, "libwebsocket gtfs-rt connect failed\n");
+        ret = 1;
+        goto bail;
+    }
+    fprintf(stderr, "websocket connections opened\n");
 
-	/* service the websocket context to handle incoming packets */
-	int n = 0;
-	while (n >= 0 && !socket_closed && !force_exit) n = libwebsocket_service(context, 500);
+    /* service the websocket context to handle incoming packets */
+    int n = 0;
+    while (n >= 0 && !socket_closed && !force_exit) n = libwebsocket_service(context, 500);
 
 bail:
     fprintf(stderr, "Exiting\n");
