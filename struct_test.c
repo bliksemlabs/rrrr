4cfd3fe3bbefe05ec2a093cea1a85fff42cce298
Andrew Byrd <andrew@fastmail.net>
2013-08-21 22:33:10 +0200

    demonstrate why stops column store is significant

--- struct_test.c
+++ struct_test.c
@@ -53,14 +53,27 @@ struct s4 {
     rtime_t  walk_time;  // The time when this stop was reached by walking (2nd phase)
 };
 
+struct t0 {
+    uint32_t target_stop;
+    float dist_meters;
+};
+
+struct t1 {
+    uint8_t  dist_meters;
+    uint32_t target_stop;
+};
+
 int main () {
     int n_stops = 60000;
     int n_rounds = 5;
-    printf ("size = %ld total size = %ld \n", sizeof(struct s0), n_stops * n_rounds * sizeof(struct s0));
-    printf ("size = %ld total size = %ld \n", sizeof(struct s1), n_stops * n_rounds * sizeof(struct s1));
-    printf ("size = %ld total size = %ld \n", sizeof(struct s2), n_stops * n_rounds * sizeof(struct s2));
-    printf ("size = %ld total size = %ld \n", sizeof(struct s3), n_stops * n_rounds * sizeof(struct s3));
-    printf ("size = %ld total size = %ld \n", sizeof(struct s4), n_stops * n_rounds * sizeof(struct s4));
+    printf ("s0 size = %ld, total size = %ld \n", sizeof(struct s0), n_stops * n_rounds * sizeof(struct s0));
+    printf ("s1 size = %ld, total size = %ld \n", sizeof(struct s1), n_stops * n_rounds * sizeof(struct s1));
+    printf ("s2 size = %ld, total size = %ld \n", sizeof(struct s2), n_stops * n_rounds * sizeof(struct s2));
+    printf ("s3 size = %ld, total size = %ld \n", sizeof(struct s3), n_stops * n_rounds * sizeof(struct s3));
+    printf ("s4 size = %ld, total size = %ld \n", sizeof(struct s4), n_stops * n_rounds * sizeof(struct s4));
+    printf ("t0 size = %ld, total size = %ld \n", sizeof(struct t0), n_stops * n_rounds * sizeof(struct t0));
+    printf ("t1 size = %ld, total size = %ld \n", sizeof(struct t1), n_stops * n_rounds * sizeof(struct t1));
+    printf ("t1 colstore,   total size = %ld \n", (sizeof(uint8_t) + sizeof(uint32_t)) * n_stops * n_rounds);
     return 0;
 }
 
