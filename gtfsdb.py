26d497ec8024706a68b152708bb775553a5c2840
skywave <thomaskoch@gmail.com>
2013-09-04 15:47:36 +0200

    Fix asymetric transfers

--- gtfsdb.py
+++ gtfsdb.py
@@ -356,7 +356,7 @@ FROM(
 SELECT from_stop_id, to_stop_id, transfer_type, min_transfer_time
 FROM transfers WHERE from_stop_id = ?
 UNION
-SELECT from_stop_id, to_stop_id, transfer_type, min_transfer_time
+SELECT to_stop_id as from_stop_id, from_stop_id as to_stop_id, transfer_type, min_transfer_time
 FROM transfers WHERE to_stop_id = ?) as x"""
         return self.get_cursor().execute(query, (from_stop_id,from_stop_id,)) 
 
