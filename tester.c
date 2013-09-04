475ed0a370cc1371f84c3c206e6a914d04d6d2fd
Stefan de Konink <stefan@konink.de>
2013-09-05 01:27:30 +0200

    Fix case (lacked break)

--- tester.c
+++ tester.c
@@ -86,6 +86,7 @@ int main(int argc, char **argv) {
 
                 token = strtok(NULL, delim);
             }
+	    break;
         case 'T':
             tdata_file = optarg;
             break;
