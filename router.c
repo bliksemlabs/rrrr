442add02dc0e113481146a6087822cecb3b6d551
Skywave <thakoch@gmail.com>
2013-09-24 12:36:53 +0200

    Fix comments

--- router.c
+++ router.c
@@ -513,7 +513,7 @@ bool router_route(router_t *prouter, router_request_t *req) {
                       continue; //Boarding not allowed and attemping to board
                 if (rsa_alighting != (route_stop_attributes[route_stop] & rsa_alighting)) //Alighting not allowed
                    if (req->arrive_by ? attempt_board : trip != NONE) //and we're seeking to alight
-                      continue; //Boarding not allowed and attemping to board
+                      continue; //Alighting not allowed and attemping to alight
 
                 /* If we have not yet boarded a trip on this route, see if we can board one.
                    Also handle the case where we hit a stop with an existing better arrival time. */
