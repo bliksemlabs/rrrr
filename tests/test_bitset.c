f291df04564a63295ba63542b2b50e46507ed3f7
Andrew Byrd <andrew@fastmail.net>
2013-10-01 11:11:11 +0200

    split tests into separate files, one per module

--- tests/test_bitset.c
+++ tests/test_bitset.c
@@ -1,9 +1,6 @@
 #include <check.h>
 #include <stdlib.h>
-#include <stdio.h>
 #include "../bitset.h"
-#include "../hashgrid.h"
-#include "../tdata.h"
 
 START_TEST (test_bitset) {
     uint32_t max = 50000;
@@ -22,73 +19,6 @@ Suite *make_bitset_suite (void) {
     tcase_add_test  (tc_core, test_bitset);
     suite_add_tcase (s, tc_core);
     return s;
-}
-
-static void geometry_test (latlon_t *lls, uint32_t n) {
-    for (uint32_t i = 0; i < n; ++i) {
-        latlon_dump (lls + i);
-        coord_t coord;
-        coord_from_latlon(&coord, lls + i);
-        coord.x = -coord.x;
-        latlon_t ll;
-        latlon_from_coord (&ll, &coord);
-        latlon_dump (&ll);        
-        coord_dump (&coord);
-        coord_from_latlon(&coord, &ll);
-        coord_dump (&coord);
-    }
-}
-
-START_TEST (test_hashgrid) {
-    tdata_t tdata;
-    tdata_load ("timetable.dat", &tdata); // TODO replace with independent test data
-    // geometry_test (tdata.stop_coords, tdata.n_stops);
-    HashGrid hg;
-    coord_t coords[tdata.n_stops];
-    for (uint32_t c = 0; c < tdata.n_stops; ++c) {
-        coord_from_latlon(coords + c, tdata.stop_coords + c);
-    }
-    HashGrid_init (&hg, 100, 500.0, coords, tdata.n_stops);
-    // HashGrid_dump (&hg);
-    coord_t qc;
-    coord_from_lat_lon (&qc, 52.37790, 4.89787);
-    coord_dump (&qc);
-    double radius_meters = 150;
-    HashGridResult result;
-    HashGrid_query (&hg, &result, qc, radius_meters);
-    uint32_t item;
-    double distance;
-    while ((item = HashGridResult_next_filtered(&result, &distance)) != HASHGRID_NONE) {
-        latlon_t *ll = tdata.stop_coords + item;
-        // latlon_dump (tdata.stop_coords + item);
-        printf ("%d,%f,%f,%f\n", item, ll->lat, ll->lon, distance);
-    }
-    HashGrid_teardown (&hg);
-} END_TEST
-
-Suite *make_hashgrid_suite (void) {
-    Suite *s = suite_create ("HashGrid");
-    TCase *tc_core = tcase_create ("Core");
-    tcase_add_test  (tc_core, test_hashgrid);
-    suite_add_tcase (s, tc_core);
-    return s;
-}
-
-Suite *make_master_suite (void) {
-    Suite *s = suite_create ("Master");
-    return s;
-}
-
-int main (void) {
-    int number_failed;
-    SRunner *sr;
-    sr = srunner_create (make_master_suite ());
-    srunner_add_suite (sr, make_bitset_suite ());
-    srunner_add_suite (sr, make_hashgrid_suite ());
-    srunner_run_all (sr, CK_NORMAL);
-    number_failed = srunner_ntests_failed (sr);
-    srunner_free (sr);
-    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
 
 /*
