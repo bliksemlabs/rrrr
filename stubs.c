1fa3b9d1a183bc6d34beff2e27d7ebc88872afe2
Stefan de Konink <stefan@konink.de>
2014-09-01 01:35:58 +0200

    For now I don't want to have full library functions.

---
+++ stubs.c
@@ -0,0 +1,18 @@
+#include "stubs.h"
+
+bool tdata_load_mmap(tdata_t *tdata, char* filename) { return true; }
+void tdata_close_mmap(tdata_t *tdata) {}
+
+void router_request_initialize(router_request_t *router) {}
+void router_request_dump(router_t *router, router_request_t *req) {}
+void router_request_from_epoch(router_request_t *req, tdata_t *tdata, time_t epochtime) {}
+bool router_request_reverse(router_t *router, router_request_t *req) { return true; }
+
+bool router_setup(router_t *router, tdata_t *tdata) { return true; }
+
+void router_reset(router_t *router) {}
+void router_teardown(router_t *router) {}
+bool router_route(router_t *router, router_request_t *req) { return true; }
+
+/* return: number of characters written */
+uint32_t router_result_dump(router_t *router, router_request_t *req, char *buf, uint32_t buflen) { return 0; }
---
+++ stubs.h
@@ -0,0 +1,46 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include <time.h>
+
+typedef struct latlon latlon_t;
+struct latlon {
+    float lat;
+    float lon;
+};
+
+typedef struct router_request router_request_t;
+struct router_request {
+    latlon_t from_latlon;
+    latlon_t to_latlon;
+    latlon_t via_latlon;
+    uint32_t from;
+    uint32_t to;
+    uint32_t via;
+    float walk_slack;
+    float walk_speed;
+    bool arrive_by;
+};
+
+typedef struct router_t router_t;
+struct router_t {
+};
+
+typedef struct tdata tdata_t;
+struct tdata {
+};
+
+bool tdata_load_mmap(tdata_t *tdata, char* filename);
+void tdata_close_mmap(tdata_t *tdata);
+
+void router_request_initialize(router_request_t *router);
+void router_request_dump(router_t *router, router_request_t *req);
+void router_request_from_epoch(router_request_t *req, tdata_t *tdata, time_t epochtime);
+bool router_request_reverse(router_t *router, router_request_t *req);
+
+bool router_setup(router_t *router, tdata_t *tdata);
+void router_reset(router_t *router);
+void router_teardown(router_t *router);
+bool router_route(router_t *router, router_request_t *req);
+
+/* return: number of characters written */
+uint32_t router_result_dump(router_t *router, router_request_t *req, char *buf, uint32_t buflen);
