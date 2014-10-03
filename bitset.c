69c679b3cb3801c707f997c8347134648f7c2b72
Stefan de Konink <stefan@konink.de>
2014-10-04 01:16:53 +0200

    Update bitset

--- bitset.c
+++ bitset.c
@@ -1,4 +1,7 @@
-/* Copyright 2013 Bliksem Labs. See the LICENSE file at the top-level directory of this distribution and at https://github.com/bliksemlabs/rrrr/. */
+/* Copyright 2013 Bliksem Labs.
+ * See the LICENSE file at the top-level directory of this distribution and at
+ * https://github.com/bliksemlabs/rrrr/
+ */
 
 /* bitset.c : compact enumerable bit array */
 #include "bitset.h"
