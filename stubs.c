a027ddcd65e93f7908f197004b73878169479a00
Stefan de Konink <stefan@konink.de>
2014-10-09 02:49:30 +0200

    update stubs

--- stubs.c
+++ stubs.c
@@ -8,8 +8,6 @@ void router_request_dump(router_t *router, router_request_t *req) {}
 void router_request_from_epoch(router_request_t *req, tdata_t *tdata, time_t epochtime) {}
 bool router_request_reverse(router_t *router, router_request_t *req) { return true; }
 
-bool router_setup(router_t *router, tdata_t *tdata) { return true; }
-
 void router_reset(router_t *router) {}
 void router_teardown(router_t *router) {}
 bool router_route(router_t *router, router_request_t *req) { return true; }
@@ -24,6 +22,28 @@ void memset32(uint32_t *s, uint32_t u, size_t n) {
     }
 }
 
+uint32_t rrrrandom(uint32_t limit) {
+    return (uint32_t) (limit * (random() / (RAND_MAX + 1.0)));
+}
+
 char *btimetext(rtime_t rt, char *buf) { return ""; }
 
 rtime_t epoch_to_rtime (time_t epochtime, struct tm *localtm) { return 0; }
+
+
+/*
+  Check the given request against the characteristics of the router that will be used.
+  Indexes larger than array lengths for the given router, signed values less than zero, etc.
+  can and will cause segfaults and present security risks.
+
+  We could also infer departure stop etc. from start trip here, "missing start point" and reversal problems.
+*/
+bool range_check(struct router_request *req, struct router *router) {
+    uint32_t n_stops = router->tdata.n_stops;
+    if (req->time < 0)         return false;
+    if (req->walk_speed < 0.1) return false;
+    if (req->from >= n_stops)  return false;
+    if (req->to >= n_stops)    return false;
+    return true;
+}
+
--- stubs.h
+++ stubs.h
@@ -19,55 +19,10 @@
 }
 #endif
 
-typedef enum tmode {
-    m_tram      =   1,
-    m_subway    =   2,
-    m_rail      =   4,
-    m_bus       =   8,
-    m_ferry     =  16,
-    m_cablecar  =  32,
-    m_gondola   =  64,
-    m_funicular = 128,
-    m_all       = 255
-} tmode_t;
-
-typedef enum optimise {
-    /* shortest time */
-    o_shortest  =   1,
-    /* least transfers */
-    o_transfers =   2,
-    /* everything */
-    o_all       = 255
-} optimise_t;
-
-typedef enum trip_attributes {
-    ta_none = 0,
-    ta_accessible = 1,
-    ta_toilet = 2,
-    ta_wifi = 4
-} trip_attributes_t;
-
-typedef struct router_state router_state_t;
-struct router_state {
-    rtime_t  walk_time;
-};
-
 #include "tdata.h"
-
-typedef struct router_t router_t;
-struct router_t {
-    tdata_t *tdata;
-    router_state_t *states;
-};
-
 #include "router_request.h"
 
 void tdata_close_mmap(tdata_t *tdata);
-
-bool router_setup(router_t *router, tdata_t *tdata);
-void router_reset(router_t *router);
-void router_teardown(router_t *router);
-bool router_route(router_t *router, router_request_t *req);
 
 /* return: number of characters written */
 uint32_t router_result_dump(router_t *router, router_request_t *req, char *buf, uint32_t buflen);
