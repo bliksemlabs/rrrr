0ccf402fae4f9f70516de29d81b70a56588bb4fb
Thomas Koch <thakoch@gmail.com>
2014-12-07 17:35:28 +0100

    Migrate unittest from master to ansi
    * Add check as dependency
    * Add test to cmake
    * Make test_bitset ansi-c compliant and extend test
    //TODO test_hashgrid and test_radixtree

---
+++ tests/run_tests.c
@@ -0,0 +1,26 @@
+#include <check.h>
+#include <stdlib.h>
+
+// could be in a header, but simpler here
+Suite *make_bitset_suite (void);
+//Suite *make_hashgrid_suite (void);
+//Suite *make_radixtree_suite (void);
+Suite *make_master_suite (void) {
+    Suite *s = suite_create ("Master");
+    return s;
+}
+
+int main (void) {
+    int number_failed;
+    SRunner *sr;
+    sr = srunner_create (make_master_suite ());
+    srunner_add_suite (sr, make_bitset_suite ());
+    //srunner_add_suite (sr, make_hashgrid_suite ());
+    //srunner_add_suite (sr, make_radixtree_suite ());
+    srunner_set_log (sr, "test.log");
+    srunner_run_all (sr, CK_VERBOSE); // CK_NORMAL
+    number_failed = srunner_ntests_failed (sr);
+    srunner_free (sr);
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+
