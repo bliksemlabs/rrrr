1f7c7ed5b0b3dae1c848000bc88ca4468608edaf
skywave <skywave@users.noreply.github.com>
2015-01-31 19:48:43 +0100

    Fix max_time for dynamic

--- tdata_io_v3_dynamic.c
+++ tdata_io_v3_dynamic.c
@@ -38,9 +38,9 @@
 /* Set the maximum drivetime of any day in tdata */
 void set_max_time(tdata_t *td){
     uint32_t jp_index;
-    td->max_time = UNREACHED;
+    td->max_time = 0;
     for (jp_index = 0; jp_index < td->n_journey_patterns; jp_index++){
-        if (td->journey_patterns[jp_index].max_time < td->max_time) {
+        if (td->journey_patterns[jp_index].max_time > td->max_time) {
             td->max_time = td->journey_patterns[jp_index].max_time;
         }
     }
