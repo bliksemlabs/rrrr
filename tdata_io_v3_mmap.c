52806499e8523b0fa954cf8651acff640ff82590
Stefan de Konink <stefan@konink.de>
2015-02-02 01:25:35 +0100

    Fix the loading of line_name.

--- tdata_io_v3_mmap.c
+++ tdata_io_v3_mmap.c
@@ -98,6 +98,7 @@ bool tdata_io_v3_load(tdata_t *td, char *filename) {
     load_mmap (td->base, commercial_mode_for_jp, uint16_t);
     load_mmap (td->base, physical_mode_for_line, uint16_t);
     load_mmap (td->base, line_codes, uint32_t);
+    load_mmap (td->base, line_names, uint32_t);
     load_mmap (td->base, operator_ids, uint32_t);
     load_mmap (td->base, operator_names, uint32_t);
     load_mmap (td->base, operator_urls, uint32_t);
