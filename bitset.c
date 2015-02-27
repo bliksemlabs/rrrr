3424ed6ed9542e0ec052e712f09fa526fcc116c9
Thomas Koch <thakoch@gmail.com>
2015-02-27 15:47:26 +0100

    Remove unused imports

--- bitset.c
+++ bitset.c
@@ -5,11 +5,8 @@
 
 /* bitset.c : compact enumerable bit array */
 #include "bitset.h"
-#include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
 #include <assert.h>
 
 
