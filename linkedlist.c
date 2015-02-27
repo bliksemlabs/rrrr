7648c79799f559a25ab5d74f11bc42e908cdbf89
Thomas Koch <thakoch@gmail.com>
2015-02-27 15:51:57 +0100

    Bump copy right year

--- linkedlist.c
+++ linkedlist.c
@@ -1,6 +1,6 @@
-/* Copyright 2013 Bliksem Labs.
- * See the LICENSE file at the top-level directory of this distribution and at
- * https://github.com/bliksemlabs/rrrr/
+/* Copyright 2013–2015 Bliksem Labs.
+ * See the LICENSE file at the top-level directory of this distribution and
+ * at https://github.com/bliksemlabs/rrrr/
  */
 
 /* linkedlist.c */
