d2c119368e3bac08c96cd9a92906207ef4ec0319
Thomas Koch <thakoch@gmail.com>
2015-03-01 20:32:13 +0100

    Check in new street_network header

--- street_network.h
+++ street_network.h
@@ -4,7 +4,7 @@
 #include "rrrr_types.h"
 #include "tdata.h"
 
-static bool street_network_mark_entrypoints(router_request_t *req, tdata_t *tdata);
-static bool street_network_mark_exitpoints(router_request_t *req, tdata_t *tdata);
+bool streetnetwork_stoppoint_durations(latlon_t *latlon, float walk_speed, uint16_t max_walk_distance, tdata_t *tdata,street_network_t *sn);
 
 #endif
+
