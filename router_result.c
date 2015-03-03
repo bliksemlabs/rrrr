ed3d0aa2c9940ab80b07638bc91ff39090a7da1c
Thomas Koch <thakoch@gmail.com>
2015-03-03 14:34:04 +0100

    Explain cut_off in comments

--- router_result.c
+++ router_result.c
@@ -246,7 +246,7 @@ bool router_result_to_plan (plan_t *plan, router_t *router, router_request_t *re
             spidx_t sp_index = target->stop_points[i_target];
             rtime_t duration = target->durations[i_target];
 
-            /* Skip the targets which were not reached by a vhicle in the round */
+            /* Skip the targets which were not reached by a vhicle in the round or have worse times than the cutoff */
             if (router->states_time[i_state + sp_index] == UNREACHED ||
                     (req ->arrive_by ? router->states_time[i_state + sp_index] - duration < req->time_cutoff :
                                        router->states_time[i_state + sp_index] + duration > req->time_cutoff)) continue;
