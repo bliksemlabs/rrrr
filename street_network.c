7cd6edf8a43543e1c7c90460a2041c7070a513a9
Stefan de Konink <stefan@konink.de>
2015-03-09 11:03:23 +0100

    Add some informal comments to street_network.c

--- street_network.c
+++ street_network.c
@@ -5,23 +5,34 @@ street_network_init (street_network_t *sn) {
     sn->n_points = 0;
 }
 
-bool street_network_mark_duration_to_stop_point(street_network_t *sn, spidx_t sp_index, rtime_t duration){
-    int32_t i = sn->n_points;
-    while (i){
+bool
+street_network_mark_duration_to_stop_point(street_network_t *sn,
+                                           spidx_t sp_index,
+                                           rtime_t duration) {
+    uint32_t i = sn->n_points;
+
+    /* A stop point already exists in the list, only duration is updated. */
+    while (i) {
         --i;
-        if (sn->stop_points[i] == sp_index){
+        if (sn->stop_points[i] == sp_index) {
             sn->durations[i] = duration;
             return true;
         }
     }
-    if (i >= RRRR_MAX_ENTRY_EXIT_POINTS){
+
+    /* An additional duration should be added, is there enough room? */
+    if (i >= RRRR_MAX_ENTRY_EXIT_POINTS) {
         return false;
     }
+
     sn->stop_points[sn->n_points] = sp_index;
     sn->durations[sn->n_points] = duration;
     ++sn->n_points;
+
     #ifdef RRRR_DEBUG
-    printf("STREET sp_index: %d, duration %d seconds\n", sp_index, duration);
+    fprintf(stderr, "STREET sp_index: %d, duration %d seconds\n",
+                    sp_index, duration);
     #endif
+
     return true;
 }
