f6df830c35dd40157f1cfcc86e39ea4fae9bfd00
Stefan de Konink <stefan@konink.de>
2015-03-13 10:37:10 +0100

    @Skywave I don't know why this fails and different numbers pop up. Needs more work.

--- tests/run_tests.c
+++ tests/run_tests.c
@@ -4,15 +4,12 @@
 /* could be in a header, but simpler here */
 Suite *make_bitset_suite (void);
 Suite *make_geometry_suite (void);
+Suite *make_hashgrid_suite (void);
 Suite *make_json_suite (void);
 Suite *make_polyline_suite (void);
 Suite *make_radixtree_suite (void);
 Suite *make_street_network_suite (void);
 Suite *make_util_suite (void);
-
-#if 0
-Suite *make_hashgrid_suite (void);
-#endif
 
 static Suite *make_master_suite (void) {
     Suite *s = suite_create ("Master");
@@ -26,13 +23,11 @@ int main (void) {
     srunner_add_suite (sr, make_bitset_suite ());
     srunner_add_suite (sr, make_geometry_suite ());
     srunner_add_suite (sr, make_json_suite ());
+    srunner_add_suite (sr, make_hashgrid_suite ());
     srunner_add_suite (sr, make_polyline_suite ());
     srunner_add_suite (sr, make_radixtree_suite ());
     srunner_add_suite (sr, make_street_network_suite ());
     srunner_add_suite (sr, make_util_suite ());
-    #if 0
-    srunner_add_suite (sr, make_hashgrid_suite ());
-    #endif
     srunner_set_log (sr, "test.log");
     srunner_run_all (sr, CK_VERBOSE); /* CK_NORMAL */
     number_failed = srunner_ntests_failed (sr);
