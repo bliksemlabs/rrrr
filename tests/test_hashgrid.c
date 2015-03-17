7f035802864237482b49d525109a26f574f8cbcd
Paul Wagener <mail@paulwagener.nl>
2015-03-17 20:19:20 +0100

    Added unit tests for hashgrid fix
    This tests for a possible segfault that was fixed in the previous commit
    Note that the testcase can also pass the bugged code, depending on the contents
    of the memory that is out-of-bounds of the array. Running the test multiple times
    increases the chances of trapping the bug.

--- tests/test_hashgrid.c
+++ tests/test_hashgrid.c
@@ -43,12 +43,34 @@ START_TEST (test_hashgrid)
     }
 END_TEST
 
+START_TEST (test_hashgrid_init)
+    {
+        coord_t coords[2];
+        coord_from_lat_lon(&coords[0], 1.0, 1.0);
+        coord_from_lat_lon(&coords[1], 2.0, 2.0);
+
+        hashgrid_t hg;
+        hashgrid_result_t result;
+
+        hashgrid_init(&hg, 100, 500, coords, 2);
+        hashgrid_query (&hg, &result, coords[1], 500.0);
+
+        double distance = 0.0;
+        while(hashgrid_result_next_filtered(&result, &distance) != HASHGRID_NONE) {
+
+        }
+
+        hashgrid_teardown (&hg);
+    }
+END_TEST
+
 Suite *make_hashgrid_suite(void);
 
 Suite *make_hashgrid_suite(void) {
     Suite *s = suite_create("hashgrid");
     TCase *tc_core = tcase_create("Core");
     tcase_add_test  (tc_core, test_hashgrid);
+    tcase_add_loop_test  (tc_core, test_hashgrid_init, 0, 20);
     suite_add_tcase(s, tc_core);
     return s;
 }
