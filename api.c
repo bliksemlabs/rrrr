617034266af773ed9f727a89e78364ccfec5aa99
Thomas Koch <thakoch@gmail.com>
2015-03-20 23:41:46 +0100

    Deduplicate router-requests (i_rev)

--- api.c
+++ api.c
@@ -220,7 +220,7 @@ bool router_route_full_reversal (router_t *router, router_request_t *req, plan_t
     n_req++;
 
     /* first reversal, always required */
-    if (!router_request_reverse_plan (router, &req_storage[i_rev], req_storage, &n_req, &work_plan)) {
+    if (!router_request_reverse_plan (router, &req_storage[i_rev], req_storage, &n_req, &work_plan, i_rev)) {
         return false;
     }
 
