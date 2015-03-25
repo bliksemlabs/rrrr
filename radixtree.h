e0b9ddf296e316e84d646cf866ca1b6b7b8a8e04
Stefan de Konink <stefan@konink.de>
2015-03-25 23:27:38 +0100

    Fix RRRR_DEBUG function definitions for radixtree.c

--- radixtree.h
+++ radixtree.h
@@ -55,9 +55,9 @@ uint32_t radixtree_find_exact (radixtree_t *r, const char *key);
 uint32_t radixtree_find_prefix (radixtree_t *r, const char *key, rxt_edge_t *result);
 
 #ifdef RRRR_DEBUG
-uint32_t radixtree_edge_count (rxt_edge_t *e);
+uint32_t rxt_edge_count (rxt_edge_t *e);
 
-void radixtree_edge_print (rxt_edge_t *e);
+void rxt_edge_print (rxt_edge_t *e);
 #endif
 
 #endif /* _RADIXTREE_H */
