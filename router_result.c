beec744288afae1fdcab3682339220149f97e8e5
Stefan de Konink <stefan@konink.de>
2015-07-24 21:32:52 +0200

    Add const where required.

--- router_result.c
+++ router_result.c
@@ -202,8 +202,8 @@ static bool check_plan_invariants(plan_t *plan) {
 
 static int
 compareItineraries(const void *elem1, const void *elem2) {
-    itinerary_t *i1 = (itinerary_t *) elem1;
-    itinerary_t *i2 = (itinerary_t *) elem2;
+    const itinerary_t *i1 = (const itinerary_t *) elem1;
+    const itinerary_t *i2 = (const itinerary_t *) elem2;
 
     return ((i1->legs[0].t0 - i2->legs[0].t0) << 3) +
             i1->legs[i1->n_legs - 1].t1 - i2->legs[i2->n_legs - 1].t1;
