15e0ce58c6f119ed84785da7cefb23533905700d
Stefan de Konink <stefan@konink.de>
2015-08-07 17:39:53 +0200

    Calculate the global maximum time to replace set_max_time.

--- index_journey_patterns.c
+++ index_journey_patterns.c
@@ -2,9 +2,12 @@
 #include "index_vehicle_journeys.h"
 #include <stdlib.h>
 
-bool index_journey_patterns (const tdata_t *td, calendar_t **jp_active, rtime_t **jp_min, rtime_t **jp_max) {
+bool index_journey_patterns (const tdata_t *td, calendar_t **jp_active,
+                             rtime_t **jp_min, rtime_t **jp_max,
+                             rtime_t *td_max) {
     calendar_t *active = NULL;
     rtime_t *min = NULL, *max = NULL;
+    rtime_t max_time = 0;
     jpidx_t i;
 
     active = (calendar_t *) malloc (sizeof(calendar_t) * td->n_journey_patterns);
@@ -17,11 +20,13 @@ bool index_journey_patterns (const tdata_t *td, calendar_t **jp_active, rtime_t 
     while (i) {
         i--;
         index_vehicle_journeys (td, i, active, min, max);
+        if (max_time < max[i]) max_time = max[i];
     }
 
     *jp_active = active;
     *jp_min = min;
     *jp_max = max;
+    *td_max = max_time;
 
     return true;
 
@@ -30,4 +35,22 @@ failure:
     free(min);
     free(max);
     return false;
+}
+
+bool index_max_time (const tdata_t *td, rtime_t *td_max) {
+    jpidx_t i = (jpidx_t) td->n_journey_patterns;
+    rtime_t max_time = 0;
+
+    if (td->journey_pattern_max == NULL) return false;
+
+    while (i) {
+        i--;
+        if (max_time < td->journey_pattern_max[i]) {
+            max_time = td->journey_pattern_max[i];
+        }
+    }
+
+    *td_max = max_time;
+
+    return true;
 }
--- index_journey_patterns.h
+++ index_journey_patterns.h
@@ -1,4 +1,13 @@
 #include "rrrr_types.h"
 #include "tdata.h"
 
-bool index_journey_patterns (const tdata_t *td, calendar_t **jp_active, rtime_t **jp_min, rtime_t **jp_max);
+/* Returns the minimum and maximum rtime_t per journey pattern,
+ * and the global maximum rtime_t.
+ */
+bool index_journey_patterns (const tdata_t *td, calendar_t **jp_active,
+                             rtime_t **jp_min, rtime_t **jp_max,
+                             rtime_t *td_max);
+
+/* Returns the global maximum rtime_t.
+ */
+bool index_max_time (const tdata_t *td, rtime_t *td_max);
