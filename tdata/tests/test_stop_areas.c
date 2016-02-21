cf88ad74287c3661af2ecfee868517329bde5368
Stefan de Konink <stefan@konink.de>
2016-02-21 01:13:05 +0100

    return 0

--- tdata/tests/test_stop_areas.c
+++ tdata/tests/test_stop_areas.c
@@ -13,22 +13,24 @@ int main(int argv, char *args[]) {
 
     tdata_string_pool_init (&pool);
     tdata_stop_areas_init (&sas, &pool);
-    
+
     printf("now: %d %d\n", sas.len, sas.size);
 
-    { 
+    {
         const char *id[] = {"12", "13"};
         const char *name[] = {"SA1", "SA2"};
         const latlon_t ll[] = {{0.0, 0.0}, {1.0, 0.0}};
         const char *time_zone[] = {"Europe/Amsterdam", "Europe/Amsterdam"};
         tdata_stop_areas_add (&sas, id, name, ll, time_zone, 2, &offset);
     }
-    
+
     printf("now: %d %d %d\n", sas.len, sas.size, offset);
-    
+
     printf("%s %s %s\n", &sas.pool->pool[sas.stop_area_ids[0]], &sas.pool->pool[sas.stop_area_nameidx[0]], &sas.pool->pool[sas.stop_area_timezones[0]]);
     printf("%s %s %s\n", &sas.pool->pool[sas.stop_area_ids[1]], &sas.pool->pool[sas.stop_area_nameidx[1]], &sas.pool->pool[sas.stop_area_timezones[1]]);
 
     tdata_stop_areas_mrproper (&sas);
     tdata_string_pool_mrproper (&pool);
+
+    return 0;
 }
--- tdata/tests/test_stop_points.c
+++ tdata/tests/test_stop_points.c
@@ -11,24 +11,26 @@ int main(int argv, char *args[]) {
 
     tdata_string_pool_init (&pool);
     tdata_stop_points_init (&sps, &pool);
-    
+
     printf("now: %d %d\n", sps.len, sps.size);
 
-    { 
+    {
         const char *id = "12";
         const char *name = "Test1234";
         const char *platformcode = "AB";
         const latlon_t ll = {0.0, 0.0};
         const rtime_t waittime = 2;
         const spidx_t stoparea = 3;
-        const uint8_t attributes = 0; 
+        const uint8_t attributes = 0;
         tdata_stop_points_add (&sps, &id, &name, &platformcode, &ll, &waittime, &stoparea, &attributes, 1, NULL);
     }
-    
+
     printf("now: %d %d\n", sps.len, sps.size);
-    
+
     printf("%s %s %s\n", &sps.pool->pool[sps.platformcodes[0]], &sps.pool->pool[sps.stop_point_ids[0]], &sps.pool->pool[sps.stop_point_nameidx[0]]);
 
     tdata_stop_points_mrproper (&sps);
     tdata_string_pool_mrproper (&pool);
+
+    return 0;
 }
